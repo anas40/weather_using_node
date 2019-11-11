8bf0650f14fe534ffb07a593369470191384a81f
Anas <anasbeg50@gmail.com>
2019-11-11 19:42:30 +0530

    remove unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('server side js is loaded and running')
-
 // fetch('http://puzzle.mead.io/puzzle').then((response)=>{
 //     response.json().then((data)=>{
 //         console.log(data)
